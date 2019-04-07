ee4ad37a218b3aad1b7884887b45d38f3388a03f
Primajin <github@eyesbound.com>
2019-04-07 21:04:40 +0200

    add test call

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import {ThemeProvider} from 'react-jss'
+import { ThemeProvider } from 'react-jss';
 
 import './App.css';
 import theme from './theme';
-import RootComponent from './RootComponent'
+import RootComponent from './RootComponent';
+import TestComponent from './TestComponent';
 
-const App = () => <ThemeProvider theme={theme}><RootComponent/></ThemeProvider>;
+const App = () =>
+  <ThemeProvider theme={theme}>
+    <>
+      <RootComponent/>
+      <TestComponent/>
+    </>
+  </ThemeProvider>;
 
 export default App;
