834c6d21f9f2a7bea86cfdb29ad760d0ee10064d
Jannis Hell <Primajin@users.noreply.github.com>
2019-04-20 15:14:04 +0200

    add breakpoints

--- src/theme.js
+++ src/theme.js
@@ -2,6 +2,13 @@ const acpRed = '#e34142';
 const mediumGrey = '#999';
 
 const theme = {
+  breakpoints: {
+    xs: '@media (max-width: 575px)',
+    sm: '@media (min-width: 576px)',
+    md: '@media (min-width: 768px)',
+    lg: '@media (min-width: 992px)',
+    xl: '@media (min-width: 1200px)'
+  },
   colors: {
     primary: acpRed,
     secondary: mediumGrey
