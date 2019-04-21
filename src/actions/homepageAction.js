91511ec6b12665fbf208deb66f12acf6211f0bd9
Jannis Hell <Primajin@users.noreply.github.com>
2019-04-21 16:00:08 +0200

    add transform API for homepage

--- src/actions/homepageAction.js
+++ src/actions/homepageAction.js
@@ -1,10 +1,44 @@
 import axios from 'axios';
 import normalize from 'json-api-normalizer';
+import { camelCase, mapValues } from 'lodash';
 
 import { apiUrl, homeError, homeFetch, homeLoading } from '../constants';
-import { transformApiData } from '../utils';
 
 const endpoint = 'node/article';
+
+export const transformApiDataForHomepage = (rawData, endpoint) => {
+  const endPoint = camelCase(endpoint);
+  let transformedData = {};
+
+  if (
+    rawData.hasOwnProperty('meta') &&
+    rawData.meta.hasOwnProperty(endpoint) &&
+    rawData.meta[endpoint].hasOwnProperty('meta')
+  ) {
+    transformedData.count = rawData.meta[endpoint].meta.count;
+  }
+
+  if (rawData.hasOwnProperty(endPoint)) {
+    const data = rawData[endPoint];
+    const results = mapValues(data, object => {
+      const loopData = object.relationships.fieldLoop.data;
+      let url = undefined;
+
+      if (
+        rawData.hasOwnProperty(loopData.type) &&
+        rawData[loopData.type].hasOwnProperty(loopData.id)
+      ) {
+        url = rawData[loopData.type][loopData.id].attributes.uri.url;
+      }
+
+      return { ...object.attributes, url };
+    });
+
+    transformedData = { ...transformedData, results };
+  }
+
+  return { ...transformedData };
+};
 
 export const getHomepageItems = dispatch => {
   dispatch({
@@ -16,13 +50,16 @@ export const getHomepageItems = dispatch => {
     .get(
       apiUrl +
         endpoint +
-        '?include=field_loop&fields%5Bnode--article%5D=title,created,field_loop&fields%5Bfile--file%5D=uri,url&filter%5Bloop%5D%5Bpath%5D=field_loop.id&filter%5Bloop%5D%5Boperator%5D=%3C%3E&filter%5Bloop%5D%5Bvalue%5D=0'
+        '?include=field_loop&fields%5Bnode--article%5D=title,created,field_loop,field_subheadline&fields%5Bfile--file%5D=uri,url&filter%5Bloop%5D%5Bpath%5D=field_loop.id&filter%5Bloop%5D%5Boperator%5D=%3C%3E&filter%5Bloop%5D%5Bvalue%5D=0'
     )
     .then(({ data }) => {
       dispatch({
         type: homeFetch,
         payload: {
-          data: transformApiData(normalize(data, { endpoint }), endpoint)
+          data: transformApiDataForHomepage(
+            normalize(data, { endpoint }),
+            endpoint
+          )
         }
       });
 
--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-// import { keys } from 'lodash';
-
-export const transformApiData = (data, endpoint) => {
-  return { ...data, count: data.meta[endpoint].meta.count };
-};
